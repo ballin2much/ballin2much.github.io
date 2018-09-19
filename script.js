3510a1ec4914b484b7a092f4ccd50740f0515a30
ballin2much <smomark12@gmail.com>
2018-09-18 21:47:50 -0400

    Added random quote generatros

--- script.js
+++ script.js
@@ -21,5 +21,28 @@ $(window).scroll(function() {
     } else {
         $(".mainLink").removeClass("color3");
     }
-    
-});
+})
+
+window.onload = function() {
+    getRandomQuote("HS"); 
+    getRandomQuote("ITWILL"); 
+    getRandomQuote("almost"); 
+}
+
+function getRandomQuote(x) {
+    let y;
+    if (x == "HS") {
+        y = ["You don't know what it's like to be me<br>I would die for you, it's that deep<br>and you don't know what it's like to be me<br>It's so cold at night when I can't sleep<br>and you don't know what it's like to be me<br>it gets old when it's right there but I can't reach",
+        "I ain't a walking, I'm a lying contradiction<br>Sliding on the friction<br>Riding with your missus, but I just want commitment<br>I just want her kisses, and a white picket<br>Her and my children<br>I ain't a walking, I'm a lying contradiction",
+        "I don't get much sleep<br>I'm depressed, but it ain't that deep<br>I'm just looking for a place to rest my feet<br>I don't smile too much, but that's just my luck<br>I ain't stressed that death might come<br>"];
+    } else if (x == "ITWILL") {
+        y = ["Now my heart sags like my eyelids sink<br>You asking what did I mean?<br>To be honest, not to deceive<br>Messing up more, trying to brush it off my sleeve<br>so I stuck it on a beat<br>I been like this since I was sucking on a teat",
+            "Want the answers only time can tell<br>Waiting for this cancer in my mind to heal<br>Wonder where my grandmama is<br>and if she got her legs, or got some wings instead<br>and I'm still thinking of her death, still dreaming of her breath<br>",
+            "Not that anyone listens<br>This is just my therapy session<br>I got things wrong with me, I ain't scared to admit it<br>I see the harmony, it's all clear in my vision<br>Don't worry about me, self-hatred part of my instincts"]
+    } else if (x == "almost") {
+        y = ["Why are we here?<br>Why weren't you there?<br>Why should I care?<br>Why's my grandmama dead?<br>Why'd she have MS?<br>Why I feel this stress?<br>Why I don't feel blessed?<br>Why I shouldn't fear death",
+        "I ain't almost a trap rapper, I'm almost a trash rapper<br>Most of my jokes almost had laughter<br>When I look at ya I lose my breath like I have asthma<br>A lot of questions I almost asked ya<br>Actually nevermind<br>I'll save em for a better time",
+        "we all want everything nothing less<br>but lets just ride with it, vibe with it<br>trust me I get it<br>I'm wicked, I'm twisted, I'm vicious<br>I said some awful things and I meant em"]
+    }
+    document.getElementById(x).innerHTML = y[Math.floor((Math.random() * y.length))];
+}
