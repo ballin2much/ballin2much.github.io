db02652732dab6cbdccfdf9ff4d55306ba51d97f
Ballin2much <smomark12@gmail.com>
2020-12-07 22:55:58 -0500

    Added I'll Be Muttley I guess

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $(window).scroll(function() {
     var scroll1 = $('.section1').position();
     var scroll2 = $('.section2').position();
     var scroll3 = $('.section3').position();
+    var scroll4 = $('.section4').position();
     
     if (scroll >= scroll1.top && scroll <= scroll2.top) {
         $(".mainLink").addClass("color1");
@@ -21,12 +22,19 @@ $(window).scroll(function() {
     } else {
         $(".mainLink").removeClass("color3");
     }
+
+    if (scroll >= scroll4.top) {
+        $(".mainLink").addClass("color4");
+    } else {
+        $(".mainLink").removeClass("color4");
+    }
 })
 
 window.onload = function() {
     getRandomQuote("HS"); 
     getRandomQuote("ITWILL"); 
     getRandomQuote("almost"); 
+    getRandomQuote("muttley"); 
 }
 
 function getRandomQuote(x) {
@@ -43,6 +51,10 @@ function getRandomQuote(x) {
         y = ["Why are we here?<br>Why weren't you there?<br>Why should I care?<br>Why's my grandmama dead?<br>Why'd she have MS?<br>Why I feel this stress?<br>Why I don't feel blessed?<br>Why I shouldn't fear death",
         "I ain't almost a trap rapper, I'm almost a trash rapper<br>Most of my jokes almost had laughter<br>When I look at ya I lose my breath like I have asthma<br>A lot of questions I almost asked ya<br>Actually nevermind<br>I'll save em for a better time",
         "We all want everything nothing less<br>But lets just ride with it, vibe with it<br>Trust me I get it<br>I'm wicked, I'm twisted, I'm vicious<br>I said some awful things and I meant em"]
+    } else if (x == "muttley") {
+        y = ["You're a shadow, You're a ghoul<br>You're the chair in my room<br>You're the air I consume<br>You're the glare in my screen, you're the prophet of doom",
+        "I just wanna rock a kangol, and breakdance with the crew<br>I just wanna fat chain with some fat ass jewels<br>I just wanna sell crack and gangbang with my dudes<br>I just wanna sip lean and OD<br>I just don't want anyone to know me<br>I just wanna do this rap thing without cold feet"
+        ]
     }
     document.getElementById(x).innerHTML = y[Math.floor((Math.random() * y.length))];
 }
